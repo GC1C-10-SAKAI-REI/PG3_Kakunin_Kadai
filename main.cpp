6f6a4356342c528f50a7be5c963b2857afa76602
GC1C_10_サカイレイ <k019g1570@g.neec.ac.jp>
2023-06-09 12:03:03 +0900

    確認課題3

--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 
 int main()
 {
+	const int wordVal = 6;
+	const char* str[wordVal] = { "creater's","music" ,"tecnology" ,"it","design" ,"sports & health" };
 
+	for (int i = 0; i < wordVal; i++)
+	{
+		printf("%s\n", str[i]);
+	}
 
 	return 0;
 }
