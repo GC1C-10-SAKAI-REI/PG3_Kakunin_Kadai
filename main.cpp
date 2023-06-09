392e7b3de9b85be516897dbe99c22f8352241b65
GC1C_10_サカイレイ <k019g1570@g.neec.ac.jp>
2023-06-09 11:59:35 +0900

    確認課題2_2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,39 @@
 #include<stdio.h>
 
+//配列の値を反転させる関数
+void Reverse(int c[], int n);
+
 int main()
 {
+	const int colorVal = 7;
+	int color[colorVal] = { 0,1,2,3,4,5,6 };
 
+	printf("全ての配列の値の表示\n");
+	for (int i = 0; i < colorVal; i++)
+	{
+		printf("color[%d] = %d\n", i, color[i]);
+	}
+	printf("\n");
+
+	//反転
+	Reverse(color, colorVal);
+
+	printf("反転した配列の値の表示\n");
+	for (int i = 0; i < colorVal; i++)
+	{
+		printf("color[%d] = %d\n", i, color[i]);
+	}
+	printf("\n");
 
 	return 0;
+}
+
+void Reverse(int c[], int n)
+{
+	for (int i = 0; i < n / 2; i++)
+	{
+		int copy = c[i];
+		c[i] = c[n - i - 1];
+		c[n - i - 1] = copy;;
+	}
 }
