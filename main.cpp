3bf7168719568347558fca1d4f7d49ec93d26e75
GC1C_10_サカイレイ <k019g1570@g.neec.ac.jp>
2023-06-09 11:57:44 +0900

    確認課題2_1

--- main.cpp
+++ main.cpp
@@ -2,7 +2,29 @@
 
 int main()
 {
+	const int colorVal = 7;
+	int color[colorVal] = { 0,1,2,3,4,5,6 };
 
+	printf("全ての配列の値の表示\n");
+	for (int i = 0; i < colorVal; i++)
+	{
+		printf("color[%d] = %d\n", i, color[i]);
+	}
+	printf("\n");
+
+	printf("全ての配列のアドレスを表示(&使用)\n");
+	for (int i = 0; i < colorVal; i++)
+	{
+		printf("color[%d] = %p\n", i, &color[i]);
+	}
+	printf("\n");
+
+	printf("全ての配列のアドレスを表示(&不使用)\n");
+	for (int i = 0; i < colorVal; i++)
+	{
+		printf("color[%d] = %p\n", i, color + i);
+	}
+	printf("\n");
 
 	return 0;
 }
