7a6ab6f2bae92b74eaadba3f25cea6d3cdada31e
GC1C_10_サカイレイ <k019g1570@g.neec.ac.jp>
2023-06-09 12:27:16 +0900

    確認課題5

--- main.cpp
+++ main.cpp
@@ -1,8 +1,38 @@
 #include<stdio.h>
 
+enum Box
+{
+	TREASURE,
+	NONE,
+	MIMIC
+};
+
 int main()
 {
+	Box box = NONE;
 
+	printf("宝箱を見つけた、0～2で一つ選ぼう\n");
+
+	scanf_s("%d", &box);
+
+	switch (box)
+	{
+	case TREASURE:
+		printf("宝の山だ\n");
+		break;
+
+	case NONE:
+		printf("からっぽだった");
+		break;
+
+	case MIMIC:
+		printf("ミミックだ、かわi...");
+		break;
+
+	default:
+		printf("急に天井が崩れ生き埋めになった");
+		break;
+	}
 
 	return 0;
 }
