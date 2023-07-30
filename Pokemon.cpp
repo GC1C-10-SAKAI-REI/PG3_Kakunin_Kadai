e979e032c339dc7279ea4276d8d413b54626038d
GC1C_10_サカイレイ <k019g1570@g.neec.ac.jp>
2023-07-30 20:20:54 +0900

    メンバ関数の定義

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -1,14 +1,45 @@
 #include "Pokemon.h"
+#include <iostream>
+
+Pokemon::Pokemon()
+{
+	SetName("");
+}
 
 Pokemon::Pokemon(const char* name)
 {
-	this->_name = name;
-	printf("%sがあらわれた！\n", this->_name);
+	SetName(name);
+	Encounter();
 }
 
-Pokemon& Pokemon::operator=(const Pokemon& obj)
+Pokemon::Pokemon(const Pokemon& copyPoke)
 {
-	_name = obj._name;
-	// TODO: return ステートメントをここに挿入します
-	return *this;
+	printf("\nコピーします\n\n");
+	GetName();
+	SetName(copyPoke._name.c_str());
+	Encounter();
+}
+
+Pokemon::~Pokemon()
+{
+	if (_pokeJr != nullptr)
+	{
+		delete _pokeJr;
+	}
+}
+
+void Pokemon::SetName(const char* name)
+{
+	this->_name = std::string(name);
+}
+
+void Pokemon::Encounter()
+{
+	std::cout << _name << "があらわれた！" << std::endl;
+}
+
+void Pokemon::Birth()
+{
+	std::string name = this->GetName() + ".Jr";
+	_pokeJr = new Pokemon(name.c_str());
 }
